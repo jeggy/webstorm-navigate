c1a0347d4a632ef40ddc0279e2b469fc1b4ec2ca
Jógvan Olsen <jo@jebster.net>
2017-10-21 21:21:48 +0200

    test project

---
+++ src/Words/index.js
@@ -0,0 +1,5 @@
+import other from './OtherWord';
+import some from './SomeWord';
+
+export const firstWord = some;
+export const secondWord = other;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { firstWord, secondWord } from "./Words/index";
+
+
+console.log(`${firstWord} ${secondWord}!`);
